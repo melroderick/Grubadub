b050d74485ebf3d22bd74adeba08e8c249be53ed
Michael Gillett <mgillett54@gmail.com>
2015-04-03 15:37:02 -0400

    - Initial Middleman tests - Various Spring cleaning

--- grubadub/src/main/java/edu/brown/cs/grubadub/MiddleMan.java
+++ grubadub/src/main/java/edu/brown/cs/grubadub/MiddleMan.java
@@ -5,6 +5,7 @@ import java.util.List;
 import edu.brown.cs.food.DetailedRestaurant;
 import edu.brown.cs.food.Restaurant;
 import edu.brown.cs.food.RestaurantFinder;
+import edu.brown.cs.map.BoundingBox;
 import edu.brown.cs.map.LatLng;
 import edu.brown.cs.map.Route;
 import edu.brown.cs.map.RouteFinder;
@@ -18,11 +19,22 @@ public class MiddleMan {
     this.map = map;
   }
 
+  protected Route getRoute(LatLng curLoc, String destination) {
+    return map.getRoute(curLoc, destination);
+  }
+
+
   public List<Restaurant> getRestaurants(LatLng curLoc, String destination, int minutes) {
-    Route route = map.getRoute(curLoc, destination);
-    List<Restaurant> restaurants = food.findRestaurants(null);
+    Route route = getRoute(curLoc, destination);
+
+    BoundingBox bb = null;
+    List<Restaurant> restaurants = findRestaurants(bb);
 
     return restaurants;
+  }
+
+  protected List<Restaurant> findRestaurants(BoundingBox bb) {
+    return food.findRestaurants(bb);
   }
 
   public DetailedRestaurant getRestaurantDetails(String id) {
---
+++ grubadub/src/test/java/edu/brown/cs/grubadub/TestRestaurantFinder.java
@@ -0,0 +1,43 @@
+package edu.brown.cs.grubadub;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.brown.cs.food.DetailedRestaurant;
+import edu.brown.cs.food.Restaurant;
+import edu.brown.cs.food.RestaurantFinder;
+import edu.brown.cs.map.BoundingBox;
+
+public class TestRestaurantFinder implements RestaurantFinder {
+  private Restaurant[] restaurants;
+  private DetailedRestaurant[] details;
+
+  public TestRestaurantFinder(
+      Restaurant[] seedRestaurants,
+      DetailedRestaurant[] seedDetails) {
+     restaurants = seedRestaurants;
+     details = seedDetails;
+  }
+
+  @Override
+  public List<Restaurant> findRestaurants(BoundingBox bb) {
+    List<Restaurant> found = new ArrayList<>();
+
+    for (Restaurant r : restaurants) {
+      if (bb.contains(r.getLatLng())) {
+        found.add(r);
+      }
+    }
+    return found;
+  }
+
+  @Override
+  public DetailedRestaurant detailedRestaurantForID(String id) {
+    for (DetailedRestaurant d : details) {
+      if (d.getId().equals(id)) {
+        return d;
+      }
+    }
+    throw new IllegalArgumentException("Restaurant ID not found.");
+  }
+}
