cf6f9e8e1abf3f73951d88d30933b59035b7ccd1
Max Luzuriaga <max@luzuriaga.com>
2015-05-02 18:38:15 -0400

    Displaying time added in detail view.

--- grubadub/src/main/resources/static/shared/js/views/detail-view.js
+++ grubadub/src/main/resources/static/shared/js/views/detail-view.js
@@ -2,12 +2,23 @@ var app = app || {};
 
 app.DetailView = Backbone.View.extend({
 	render: function(callback) {
-		app.getTemplate("restaurants/detail", function(file) {
-			var template = _.template(file);
-			var html = template({ restaurant: this.restaurant, ror: this.restaurantOnRoute, currentLoc: app.currentLoc });
+		var draw = function() {
+			app.getTemplate("restaurants/detail", function(file) {
+				var template = _.template(file);
+				var html = template({ restaurant: this.restaurant, ror: this.restaurantOnRoute, currentLoc: app.currentLoc });
 
-			$(this.el).html(html);
-			callback(this);
-		}.bind(this));
+				$(this.el).html(html);
+				callback(this);
+			}.bind(this));
+		}.bind(this);
+
+		if (this.restaurantOnRoute.get('timeAdded')) {
+			draw();
+		} else {
+			$.get('/time', {lat: app.currentLoc.lat, lng: app.currentLoc.lng, waypoint: this.restaurant.get('address'), destination: app.userDestination}, function(data) {
+				this.restaurantOnRoute.set('timeAdded', data.time);
+				draw();
+			}.bind(this));
+		}
 	}
 });
